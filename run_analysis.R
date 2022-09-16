07f6b8fcc4b4edb52418926647d17d3f425909b1
Michael C. Amakoh <21214@nnpcgroup.com>
2022-09-16 21:32:04 +0100

    implemented task 5 (average_per_activity_per_person)}))

--- run_analysis.R
+++ run_analysis.R
@@ -8,8 +8,7 @@
 # 
 #
 # 
-# 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
-
+#
 # Load Packages if required
 
 if(!require("tidyverse")){
@@ -40,7 +39,7 @@ if(!require("reshape2")){
   library(reshape2)
 }
 
-#Task 3. Uses descriptive activity names to name the activities in the data set
+#TASK 3. Uses descriptive activity names to name the activities in the data set
 current_directory<-"./"
 # reading the data from d datasets
 
@@ -53,7 +52,7 @@ features <- fread(file.path(current_directory, "UCI HAR Dataset/features.txt")
                   , col.names = c("index", "featureNames"))
 
 # get indices for rows containing either of these string format mean() or std()
-# task 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
+# TASK 2. Extracts only the measurements on the mean and standard deviation for each measurement. 
 featuresWanted <- grep("(mean|std)\\(\\)", features[, featureNames])
 # use indices obtained from the featuresWanted to extract the desired entry
 measurements <- features[featuresWanted, featureNames]
@@ -61,7 +60,7 @@ measurements <- features[featuresWanted, featureNames]
 measurements <- gsub('[()]', '', measurements)
 
 
-#task 4.Appropriately labels the data set with descriptive variable names. 
+#TASK 4.Appropriately labels the data set with descriptive variable names. 
 # reading the training dataset
 train <- fread(file.path(current_directory, "UCI HAR Dataset/train/X_train.txt"))[, featuresWanted, with = FALSE]
 #replacing the default column names of train dataset with data extracted from measurement
@@ -87,6 +86,24 @@ test_Subjects <- fread(file.path(current_directory, "UCI HAR Dataset/test/subjec
                       , col.names = c("SubjectNum"))
 test <- cbind(test_Subjects, test_Activities, test)
 
-# task 1. Merges the training and the test sets to create one data set.
+# TASK 1. Merges the training and the test sets to create one data set.
 #combining rows of test and train into a single dataset
 combined_data <- rbind(train, test) # merging the train and test dataset
+
+
+#TASK 5. From the data set in step 4, creates a second, independent tidy data set 
+#   with the average of each variable for each activity and each subject.
+
+factor_1<-factor(combined_data$SubjectNum)
+factor_2<-factor(combined_data$Activity)
+factor_combinations<-list(factor_1,factor_2)
+average_per_activity_per_person<-lapply(split(combined_data, factor_combinations),function(x) {
+                                              colMeans(x[,3:ncol(x)],
+                                                       na.rm = TRUE)})
+# converting the list data type of the variable average_per_activity_per_person to a dataframe
+# and the swap row and column fields with the t() function                                            
+
+average_per_activity_per_person <-data.frame(t(data.frame(average_per_activity_per_person)))
+
+# colMeans(combined_data[,3:ncol(combined_data)],na.rm=TRUE)
+
